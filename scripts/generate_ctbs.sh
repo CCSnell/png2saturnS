fce6ed27c2fde81643f369a8c6439e1c8ff6cb34
Christopher Snell <CCSnell@users.noreply.github.com>
2023-04-22 01:00:43 -0500

    Update generate_ctbs.sh

--- scripts/generate_ctbs.sh
+++ scripts/generate_ctbs.sh
@@ -15,7 +15,7 @@ then
     echo "Generating bottom layer ctb for $1..."
     b_png=${1%.*}.png
     b_ctb=${1%.*}.ctb
-    gerbv -D 508 -B 0 -b "#FFFFFF" -f "#000000FF" -x png -o $b_png $1
+    gerbv -D 531 -B 0 -b "#FFFFFF" -f "#000000FF" -x png -o $b_png $1
 
     convert $b_png -scale 200x125 -background white -gravity center -extent 200x125 p_small_$b_png
     convert $b_png -scale 400x300 -background white -gravity center -extent 400x300 p_large_$b_png
@@ -31,7 +31,7 @@ then
     echo "Generating top layer ctb for $2..."
     f_png=${2%.*}.png
     f_ctb=${2%.*}.ctb
-    gerbv -D 508 -B 0 -b "#FFFFFF" -f "#000000FF" -m Y -x png -o $f_png $2
+    gerbv -D 531 -B 0 -b "#FFFFFF" -f "#000000FF" -m Y -x png -o $f_png $2
 
     convert $f_png -scale 200x125 -background white -gravity center -extent 200x125 p_small_$f_png
     convert $f_png -scale 400x300 -background white -gravity center -extent 400x300 p_large_$f_png
